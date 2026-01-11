0aa9cc57fd2a0184c8b4f217f01fa649b089ca88
vivithequeen1 <vivithequeen1@gmail.com>
2026-01-11 18:27:14 -0500

    add finish up base creature, tail ect, NEXT FOR CREATURE: - more customization in creation - random colors based on pallete - different ai patterns - uhh ai is kinda important actually - change pictures and stuff

--- creatures/base_nodes/HeadBase.cs
+++ creatures/base_nodes/HeadBase.cs
@@ -10,28 +10,34 @@ public partial class HeadBase : Node2D
 
 	int AmountOfSegments = 10;
 	Vector2 TargetPosition;
-	PackedScene BodySegmentBase = GD.Load<PackedScene>("res://creatures/base_nodes/BodySegmentBase.tscn");
+	PackedScene bodySegmentBase = GD.Load<PackedScene>("res://creatures/base_nodes/BodySegmentBase.tscn");
+	PackedScene tailBase = GD.Load<PackedScene>("res://creatures/base_nodes/TailBase.tscn");
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Vector2 CurrentPosition = GlobalPosition;
-
-		for(int i = 0; i < AmountOfSegments; i++)
+		Node parent = GetParent();
+		for(int i = 0; i < AmountOfSegments-1; i++)
 		{
-			BodySegmentBase NewSegment = BodySegmentBase.Instantiate<BodySegmentBase>();
-			Node parent = GetParent();
-			if (parent != null) {
-				parent.CallDeferred(Node.MethodName.AddChild, NewSegment);
-			} else {
-				GetTree().Root.AddChild(NewSegment);
-			}
+			BodySegmentBase NewSegment = bodySegmentBase.Instantiate<BodySegmentBase>();
+			
+			
+			parent.CallDeferred(Node.MethodName.AddChild, NewSegment);
+
 
 			BodySegments.Add(NewSegment);
 			
 			NewSegment.InitSegment(CurrentPosition);
 			CurrentPosition.X-=32;
 		}
-		GD.Print(BodySegments.Count);
+		
+		BodySegmentBase NewTail = tailBase.Instantiate<BodySegmentBase>();
+		
+		
+		parent.CallDeferred(Node.MethodName.AddChild, NewTail);
+		BodySegments.Add(NewTail);
+
+		NewTail.InitSegment(CurrentPosition);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
--- creatures/base_nodes/TailBase.cs
+++ creatures/base_nodes/TailBase.cs
@@ -1,24 +1,20 @@
 using Godot;
 using System;
 
-public partial class BodySegmentBase : Node2D
+public partial class TailBase : BodySegmentBase
 {
-	public void InitSegment(Vector2 InitPosition)
-	{
-		GD.Print("I HAVE BEEN BORN");
-		GlobalPosition = InitPosition;
-	}
 
-	virtual public void FollowTarget(Vector2 TargetPosition, double delta)
+	public override void FollowTarget(Vector2 TargetPosition, double delta)
 	{
 		float DistanceToTarget = GlobalPosition.DistanceTo(TargetPosition);
 		Vector2 DirectionToTarget = GlobalPosition.DirectionTo(TargetPosition);
 		LookAt(TargetPosition);
 
 
-		if(DistanceToTarget > 64)
+		if (DistanceToTarget > 15)
 		{
 			GlobalPosition = GlobalPosition.MoveToward(TargetPosition, 300 * (float)delta);
 		}
 	}
+
 }
