ac1739889820e8202a449b6bcedec5faab80b763
officialErnests <n0o0b090lv@gmail.com>
2026-02-13 14:03:43 +0200

    pulling from main

---
+++ noob/scripts/multi/buttons.gd
@@ -0,0 +1,14 @@
+extends Button
+
+@export var scene_to: PackedScene
+@export var exit: bool
+@export var main_menu: bool
+var main_menu_node = preload("res://noob/scenes/main_menu.tscn")
+
+func _ready() -> void:
+	pressed.connect(button_pressed)
+
+func button_pressed() -> void:
+	if exit: get_tree().quit(); return;
+	if main_menu: get_tree().change_scene_to_packed(main_menu_node)
+	if scene_to: get_tree().change_scene_to_packed(scene_to)
