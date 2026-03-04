dc86f76634466cbcaec2f45a480c99e60ef36642
officialErnests <n0o0b090lv@gmail.com>
2026-03-04 21:21:59 +0200

    scarf added

--- noob/scripts/process/Scarf.cs
+++ noob/scripts/process/Scarf.cs
@@ -9,31 +9,40 @@ public partial class Scarf : Line2D
 	public int distance = 0;
 	[Export]
 	public Vector2 start_position = Vector2.Zero;
+	[Export]
+	public Vector2 gravity = Vector2.Zero;
+
+	[Export]
+	public Vector2 wind = Vector2.Zero;
 
 	private Vector2 global_offset = Vector2.Zero;
 	private List<Vector2> point_offsets;
+	private Vector2 previous_global_position;
 	public override void _Ready() 
 	{
-		global_offset = GlobalPosition / GlobalTransform.Scale;
+		global_offset = GlobalPosition;
 		point_offsets =
         [
-            GetPointPosition(3) / GlobalTransform.Scale,
-            GetPointPosition(2) / GlobalTransform.Scale,
-            GetPointPosition(1) / GlobalTransform.Scale,
-            GetPointPosition(0) / GlobalTransform.Scale,
+            GetPointPosition(0),
+            GetPointPosition(1),
+            GetPointPosition(2),
+            GetPointPosition(3),
         ];
 	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Vector2 true_global_scale = GlobalPosition / GlobalTransform.Scale;
-		Vector2 curent_position = true_global_scale - start_position;
-		for (int scarf_node_iter = 0; scarf_node_iter < point_offsets.Count; scarf_node_iter++)
+		Vector2 scarfs_velocity = (GlobalPosition - previous_global_position) / GlobalTransform.Scale;
+		previous_global_position = GlobalPosition;
+
+		Vector2 curent_position = global_offset - start_position + start_position;
+		for (int scarf_node_iter = point_offsets.Count - 1; scarf_node_iter >= 0; scarf_node_iter--)
 		{
-			Vector2 scarf_node_position = point_offsets[scarf_node_iter];
-			scarf_node_position = curent_position + ((scarf_node_position - curent_position).Normalized() * distance);
-			curent_position = scarf_node_position;
-			SetPointPosition(scarf_node_iter, scarf_node_position - true_global_scale - start_position);
+			curent_position = (point_offsets[scarf_node_iter] - curent_position).Normalized() * distance + curent_position;
+			point_offsets[scarf_node_iter] = curent_position - scarfs_velocity + (gravity + wind) * (float)delta;
+			SetPointPosition(scarf_node_iter, curent_position);
 		}
+		
+		GlobalRotation = 0;
 	}
 }
